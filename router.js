c131d50c1024bfba373f405712e434ac04a8538f
Ally Maumba <amaumba1@gmail.com>
2017-08-21 01:52:32 -0500

    Adding changes

---
+++ router.js
@@ -0,0 +1,9 @@
+const Authentication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+const requireAuth = passport.authenticate('jwt', { session: false });
+
+module.exports = function(app) {
+    app.post('/signup', Authentication.signup); 
+}
